f117d84f07c85a926a674384c3b3ed48671b1dd6
bansalayush <ayush.7scor@gmail.com>
2017-03-21 12:07:17 +0530

    Initial commit

---
+++ app/src/main/java/com/scorpio/examples/terminalcommands/TerminalCommands.java
@@ -0,0 +1,37 @@
+package com.scorpio.examples.terminalcommands;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.scorpio.examples.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class TerminalCommands extends AppCompatActivity {
+
+    /**
+     * Some of the terminal commands that are working on my device
+     * command ____ return
+     *
+     * date    ____  Mon Mar 20 11:21:28 IST 2017
+     * ls      ____  acct,bin,cache,charger,config,cust,d,data...... and many more
+     *
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terminal_commands);
+        try {
+            Process p = Runtime.getRuntime().exec("");
+            Scanner sc  = new Scanner(new InputStreamReader(p.getInputStream()));
+            while(sc.hasNext())
+            System.out.println(sc.next());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
